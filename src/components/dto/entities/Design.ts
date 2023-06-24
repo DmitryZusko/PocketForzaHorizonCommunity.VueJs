6e3c5b518f29166523d27db61d471bc1ffd2c42c
Dmitry <elxf123@gmail.com>
2023-06-25 00:32:15 +0300

    add all necessary dtos

--- src/components/dto/entities/Design.ts
+++ src/components/dto/entities/Design.ts
@@ -1,5 +1,11 @@
 export interface IDesign {
-  thumbnail: string;
+  id: string;
   title: string;
+  thumbnailUrl: string;
   description: string;
+  forzaShareCode: string;
+  rating: number;
+  authorUsername: string;
+  carModel: string;
+  creationDate: string;
 }
---
+++ src/components/dto/requests/GuideGetRequest.ts
@@ -0,0 +1,6 @@
+export interface IGuideGetRequest {
+  descriptionLimit: number;
+  searchQuery: string;
+  page: number;
+  pageSize: number;
+}
