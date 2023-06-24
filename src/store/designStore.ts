c2104ffc17492191a9bbf273f2083720e84475d5
Dmitry <elxf123@gmail.com>
2023-06-25 00:33:01 +0300

    design store

--- src/store/designStore.ts
+++ src/store/designStore.ts
@@ -1,8 +1,31 @@
+import { defaultPageSize } from "@/components/constants/defaultConstants";
+import { IDesignGetRequest } from "@/components/dto/requests/DesignGetRequest";
+import designService from "@/components/services/design-service";
 import { defineStore } from "pinia";
+import { IDesignStoreState } from "./types";
 
 export const useDesignStore = defineStore("designStore", {
-  state: () => ({
+  state: (): IDesignStoreState => ({
     isLoading: false,
-    designs: [],
+    entities: [],
+    total: 0,
+    page: 0,
+    pageSize: defaultPageSize,
   }),
+  getters: {
+    getDesignsState: (state) => state,
+  },
+  actions: {
+    async loadDesigns(params: IDesignGetRequest) {
+      try {
+        this.isLoading = true;
+        const result = await designService.loadDesigns(params);
+        this.entities = result.data.entities;
+      } catch (error) {
+        alert(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
 });
