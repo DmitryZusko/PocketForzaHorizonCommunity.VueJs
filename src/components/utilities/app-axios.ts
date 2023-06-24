62ac1b71543244a9f07ff002b481f5f20b45012e
Dmitry <elxf123@gmail.com>
2023-06-25 00:31:09 +0300

    add an app axios instance with a predefined base url

--- src/components/utilities/app-axios.ts
+++ src/components/utilities/app-axios.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import envHandler from "./env-handler";
 
 const getInstance = () => {
+  console.log(envHandler.getBaseUrl());
+
   return axios.create({
     baseURL: envHandler.getBaseUrl(),
   });
