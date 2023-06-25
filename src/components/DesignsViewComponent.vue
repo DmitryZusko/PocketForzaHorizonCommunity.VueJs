027145ce3f4a52cdf715be4df1fbd082d920efd1
Dmitry <elxf123@gmail.com>
2023-06-25 14:01:07 +0300

    1) add an infinite scroll for cards 2) organize design cards into grid 3) add all necessary behavior for a design loading

--- src/components/DesignsViewComponent.vue
+++ src/components/DesignsViewComponent.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import NavigationCard from "@/components/NavigationCard.vue";
 import { useDesignStore } from "@/store/designStore";
-import { ref, watchEffect } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { defaultTextPreviewLimit } from "@/components/constants/defaultConstants";
+import DesignCardBody from "./DesignCardBody.vue";
+import { VInfiniteScroll } from "vuetify/lib/labs/components.mjs";
 
 const designStore = useDesignStore();
 
@@ -10,19 +12,64 @@ const designState = designStore.getDesignsState;
 
 const searchQuery = ref("");
 
-watchEffect(async () => {
-  designStore.loadDesigns({
+const loadDesigns = async () => {
+  await designStore.loadDesigns({
     page: designState.page,
     pageSize: designState.pageSize,
-    searchQuery: searchQuery.value,
+    searchQuery: searchQuery.value || null,
     descriptionLimit: defaultTextPreviewLimit,
   });
+
+  designStore.setPage(++designState.page);
+};
+
+const handleInfinitLoad = async ({
+  done,
+}: {
+  done: (status: "error" | "loading" | "empty" | "ok") => void;
+}) => {
+  if (designState.total < designState.pageSize * designState.page) return done("empty");
+
+  loadDesigns();
+
+  done("ok");
+};
+
+onMounted(async () => {
+  loadDesigns();
+});
+
+onUnmounted(() => {
+  designStore.$reset();
 });
 </script>
 
 <template>
   <SearchComponent />
   <CarSelectorComponent />
-  <NavigationCard />
-  <p v-for="design in designState.entities" :key="design.title">{{ design.id }}</p>
+  <v-infinite-scroll :items="designState.entities" @load="handleInfinitLoad">
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="design in designState.entities"
+          :key="design.id"
+          cols="12"
+          sm="12"
+          md="6"
+          lg="4"
+        >
+          <NavigationCard :image-src="design.thumbnailUrl">
+            <template v-slot:card-body>
+              <DesignCardBody
+                :title="design.title"
+                :description="design.description || ''"
+                :author="design.authorUsername"
+                :creation-date="design.creationDate"
+              />
+            </template>
+          </NavigationCard>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-infinite-scroll>
 </template>
