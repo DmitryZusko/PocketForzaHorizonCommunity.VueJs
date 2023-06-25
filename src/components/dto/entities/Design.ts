d20ced2ac7f5fc94a9536d2142617a86226146d3
Dmitry <elxf123@gmail.com>
2023-06-25 13:58:11 +0300

    The description may be null

--- src/components/dto/entities/Design.ts
+++ src/components/dto/entities/Design.ts
@@ -2,7 +2,7 @@ export interface IDesign {
   id: string;
   title: string;
   thumbnailUrl: string;
-  description: string;
+  description: string | null;
   forzaShareCode: string;
   rating: number;
   authorUsername: string;
