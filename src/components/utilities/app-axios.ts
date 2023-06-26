504bdc82555aff0bc2f620a365c6a68638fa5fe0
Dmitry <elxf123@gmail.com>
2023-06-26 15:28:19 +0300

    add an authorization header for an axios instance

--- src/components/utilities/app-axios.ts
+++ src/components/utilities/app-axios.ts
@@ -1,9 +1,14 @@
+import { useAuthStore } from "@/store/authStore";
 import axios from "axios";
 import envHandler from "./env-handler";
 
 const getInstance = () => {
+  const authStore = useAuthStore();
   return axios.create({
     baseURL: envHandler.getBaseUrl(),
+    headers: {
+      Authorization: `Bearer ${authStore.getAuthState.accessToken}`,
+    },
   });
 };
 
